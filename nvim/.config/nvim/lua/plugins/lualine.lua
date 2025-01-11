a3a1a25a8b66f077d90f02e9221a83a8ec594441
woidpointer <woidpointer@gmail.com>
2025-01-11 10:14:01 +0100

    Adapted lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,18 +1,21 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    lazy = false,
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "auto",
-          section_separators = { left = "", right = "" },
-          component_separators = { left = "", right = "" },
-          disabled_filetypes = { "NvimTree" },
-        },
-      })
-    end,
+	{
+		"nvim-lualine/lualine.nvim",
+		lazy = false,
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+					section_separators = { left = "", right = "" },
+					component_separators = { left = "", right = "" },
+					disabled_filetypes = { "NvimTree" },
+				},
+				sections = {
+					lualine_c = { { "filename", path = 1 } },
+				},
+			})
+		end,
 
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 }
