52eff7ba31ec80844863d8ecabbe19f691b1a631
woidpointer <woidpointer@gmail.com>
2025-01-25 10:30:47 +0100

    Added find all files

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,25 +1,26 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    version = "*",
-    config = function()
-      require("mini.indentscope").setup()
-      require("mini.move").setup({
-        mappings = {
-          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-          left = "H",
-          right = "L",
-          down = "J",
-          up = "K",
+	{
+		"echasnovski/mini.nvim",
+		version = "*",
+		config = function()
+			require("mini.indentscope").setup()
+			require("mini.move").setup({
+				mappings = {
+					-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+					left = "H",
+					right = "L",
+					down = "J",
+					up = "K",
 
-          -- Move current line in Normal mode
-          line_left = "",
-          line_right = "",
-          line_down = "",
-          line_up = "",
-        },
-      })
-      require("mini.pairs").setup()
-    end,
-  },
+					-- Move current line in Normal mode
+					line_left = "",
+					line_right = "",
+					line_down = "",
+					line_up = "",
+				},
+			})
+			require("mini.pairs").setup()
+			require("mini.surround").setup()
+		end,
+	},
 }
