c12773fd9efa82b60c06dcf941c2a1b80590bca1
Woid Pointer <woidpointer@gmail.com>
2025-06-29 21:01:39 +0200

    Added own keymapping for blink

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -21,7 +21,20 @@ return {
 		-- C-k: Toggle signature help (if signature.enabled = true)
 		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = "default" },
+		-- keymap = { preset = "default" },
+		keymap = {
+			["<C-j>"] = { "show", "select_next", "fallback" },
+			["<C-k>"] = { "show", "select_prev", "fallback" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<C-space>"] = { "show", "select_and_accept", "fallback" },
+			["<up>"] = { "select_prev", "fallback" },
+			["<down>"] = { "select_next", "fallback" },
+			["<C-]>"] = { "show_documentation", "hide_documentation", "fallback" },
+			["<C-u>"] = { "scroll_documentation_up", "fallback" },
+			["<C-d>"] = { "scroll_documentation_down", "fallback" },
+			["<C-f>"] = { "snippet_forward", "fallback" },
+			["<C-b>"] = { "snippet_backward", "fallback" },
+		},
 
 		appearance = {
 			nerd_font_variant = "mono",
