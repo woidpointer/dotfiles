f419c4aae6efa6c7135197437dd2725fbcfb7854
Woid Pointer <woidpointer@gmail.com>
2025-06-29 20:42:02 +0200

    Added fzf-lua (first version)

---
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,180 @@
+return {
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- or if using mini.icons/mini.nvim
+		-- dependencies = { "echasnovski/mini.icons" },
+		opts = {},
+		keys = {
+			{
+				"<leader>ff",
+				function()
+					require("fzf-lua").git_files()
+				end,
+				desc = "Find git Files in project directory",
+			},
+			{
+				"<leader>fa",
+				function()
+					require("fzf-lua").files()
+				end,
+				desc = "Find all Files in project directory",
+			},
+			{
+				"<leader>fg",
+				function()
+					require("fzf-lua").live_grep()
+				end,
+				desc = "Find by grepping in project directory",
+			},
+			{
+				"<leader>fc",
+				function()
+					require("fzf-lua").files({ cwd = vim.fn.stdpath("config") })
+				end,
+				desc = "Find in neovim configuration",
+			},
+			{
+				"<leader>fh",
+				function()
+					require("fzf-lua").helptags()
+				end,
+				desc = "[F]ind [H]elp",
+			},
+			{
+				"<leader>fk",
+				function()
+					require("fzf-lua").keymaps()
+				end,
+				desc = "[F]ind [K]eymaps",
+			},
+			{
+				"<leader>fb",
+				function()
+					require("fzf-lua").builtin()
+				end,
+				desc = "[F]ind [B]uiltin FZF",
+			},
+			{
+				"<leader>fw",
+				function()
+					require("fzf-lua").grep_cword()
+				end,
+				desc = "[F]ind current [W]ord",
+			},
+			{
+				"<leader>fW",
+				function()
+					require("fzf-lua").grep_cWORD()
+				end,
+				desc = "[F]ind current [W]ORD",
+			},
+			{
+				"<leader>fd",
+				function()
+					require("fzf-lua").diagnostics_document()
+				end,
+				desc = "[F]ind [D]iagnostics",
+			},
+			{
+				"<leader>fr",
+				function()
+					require("fzf-lua").resume()
+				end,
+				desc = "[F]ind [R]esume",
+			},
+			{
+				"<leader>fo",
+				function()
+					require("fzf-lua").oldfiles()
+				end,
+				desc = "[F]ind [O]ld Files",
+			},
+			{
+				"<leader><leader>",
+				function()
+					require("fzf-lua").buffers()
+				end,
+				desc = "Find existing buffers",
+			},
+			{
+				"<leader>/",
+				function()
+					require("fzf-lua").lgrep_curbuf()
+				end,
+				desc = "[/] Live grep the current buffer",
+			},
+			{
+				"<leader>sc",
+				function()
+					require("fzf-lua").spellcheck()
+				end,
+				desc = "Misspelled words in buffer",
+			},
+			{
+				"<leader>sg",
+				function()
+					require("fzf-lua").spell_suggest()
+				end,
+				desc = "Spelling Suggesting",
+			},
+			{
+				"gd",
+				function()
+					require("fzf-lua").lsp_definitions()
+				end,
+				desc = "Goto Definition",
+			},
+			{
+				"gD",
+				function()
+					require("fzf-lua").lsp_declarations()
+				end,
+				desc = "Goto Definition",
+			},
+			{
+				"<leader>lr",
+				function()
+					require("fzf-lua").lsp_references()
+				end,
+				desc = "List References",
+			},
+			{
+				"gI",
+				function()
+					require("fzf-lua").lsp_implementations()
+				end,
+				desc = "Goto implementation",
+			},
+			{
+				"<leader>D",
+				function()
+					require("fzf-lua").lsp_typedefs()
+				end,
+				desc = "LSP Type Definitions",
+			},
+			{
+				"<leader>ca",
+				function()
+					require("fzf-lua").lsp_code_actions()
+				end,
+				desc = "Code Actions",
+			},
+			{
+				"<leader>li",
+				function()
+					require("fzf-lua").lsp_incoming_calls()
+				end,
+				desc = "List Incoming calls",
+			},
+			{
+				"<leader>lo",
+				function()
+					require("fzf-lua").lsp_outgoing_calls()
+				end,
+				desc = "List Outgoing calls",
+			},
+		},
+	},
+}
--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -10,9 +10,6 @@ return {
 			-- Useful status updates for LSP.
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{ "j-hui/fidget.nvim", opts = {} },
-
-			-- Allows extra capabilities provided by nvim-cmp
-			--"hrsh7th/cmp-nvim-lsp",
 		},
 
 		config = function()
@@ -57,52 +54,11 @@ return {
 						mode = mode or "n"
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
-
-					-- Jump to the definition of the word under your cursor.
-					--  This is where a variable was first declared, or where a function is defined, etc.
-					--  To jump back, press <C-t>.
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]definition")
-
-					-- Find references for the word under your cursor.
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-					-- Jump to the implementation of the word under your cursor.
-					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-					-- Jump to the type of the word under your cursor.
-					--  Useful when you're not sure what type a variable is and you want to see
-					--  the definition of its *type*, not where it was *defined*.
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]definition")
-
-					-- Fuzzy find all the symbols in your current document.
-					--  Symbols are things like variables, functions, types, etc.
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]symbols")
-
-					-- Fuzzy find all the symbols in your current workspace.
-					--  Similar to document symbols, except searches over your entire project.
-					map(
-						"<leader>ws",
-						require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"[W]orkspace [S]symbols"
-					)
-
 					-- Rename the variable under your cursor.
 					--  Most Language Servers support renaming across files, etc.
 					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-					-- Execute a code action, usually your cursor needs to be on top of an error
-					-- or a suggestion from your LSP for this to activate.
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-
-					-- WARN: This is not Goto Definition, this is Goto Declaration.
-					--  For example, in C this would take you to the header.
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
 					map("<leader>K", vim.lsp.buf.hover, "LSP Hover")
-					map("<leader>lr", vim.lsp.buf.references, "[L]ist [R]eferences")
-					map("<leader>li", vim.lsp.buf.references, "[L]ist [I]ncomming calls")
-					map("<leader>lo", vim.lsp.buf.references, "[L]ist [O]utgoing calls")
 
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
@@ -148,14 +104,6 @@ return {
 					end
 				end,
 			})
-
-			-- LSP servers and clients are able to communicate to each other what features they support.
-			--  By default, Neovim doesn't support everything that is in the LSP specification.
-			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-
-			-- local capabilities = vim.lsp.protocol.make_client_capabilities()
-			-- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 		end,
 	},
 }
