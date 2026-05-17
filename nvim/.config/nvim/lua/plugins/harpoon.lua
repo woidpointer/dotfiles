3c3a84982f30136cf5a992beb67139e82a3aaf7b
woidpointer <woidpointer@gmail.com>
2026-05-17 14:49:28 +0200

    Added hexview and used snacks as harpoon picker

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -3,6 +3,15 @@ return {
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
+
+		settings = {
+			save_on_toggle = false,
+			sync_on_ui_close = false,
+			-- key = function()
+			--  return vim.loop.cwd()
+			-- end,
+		},
+
 		config = function()
 			local harpoon = require("harpoon")
 			harpoon:setup({})
@@ -11,9 +20,10 @@ return {
 				harpoon:list():add()
 			end)
 
-			vim.keymap.set("n", "<leader>hl", function()
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end)
+			-- ATTETION: Keymap for list is moved to picker.lua
+			-- vim.keymap.set("n", "<leader>hl", function()
+			-- 	harpoon.ui:toggle_quick_menu(harpoon:list())
+			-- end)
 		end,
 	},
 }
